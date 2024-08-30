a8b5d8808d5d6cf3071f3d2191a0a0d733bd50c3
daus-s <dauscarmichael@gmail.com>
2024-08-30 09:17:11 -0700

    Navi struct constructor complete

--- pilot/src/pilot.rs
+++ pilot/src/pilot.rs
@@ -1,8 +1,19 @@
 use daus::vfs::{VirtualFileSystem, VirtualNode};
 
-pub struct Navi {
-    pub system: VirtualFileSystem,
-    pub path: Vec<String>,
+pub struct Navi<'a> {
+    pub fsys: &'a VirtualFileSystem,
+    pub path: Vec<String>, //change to a VirtualPath
+    pub curr: &'a VirtualNode,
+    pub prev: Option<&'a VirtualNode>,
 }
 
-impl Navi {}
+impl<'a> Navi<'a> {
+    pub fn new(fsys: &'a VirtualFileSystem) -> Self {
+        Navi {
+            fsys,
+            path: Vec::new(),
+            curr: &fsys.root,
+            prev: None,
+        }
+    }
+}
